9e85adc932faa652097ce7262799060d0bde0d74
BenjaminJacobson <ben.jacobson@flatironschool.com>
2024-05-03 18:12:41 -0400

    initial models.py User class

---
+++ server/models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+
+db = SQLAlchemy()
+
+class Users(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    greeting = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<User: id {self.id} | name {self.name} | Greeting {self.greeting} >'
+
+    
+if __name__ == "__main__":
+    u = Users(name = "test_name", greeting = "test_greeting")
+    print(u)
