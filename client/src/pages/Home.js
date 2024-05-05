a451264c598f67d94b7b25d23722ad35dbd5e824
BenjaminJacobson <ben.jacobson@flatironschool.com>
2024-05-05 15:22:35 -0400

    working local basic front end and backend, to test on render

---
+++ client/src/pages/Home.js
@@ -0,0 +1,17 @@
+import {useState, useEffect} from "react";
+// import UserCard from "../components/UserCard";
+
+function Home() {
+  const [users, setUsers] = useState([])
+
+  // useEffect(() => {
+  //   fetch("htt")
+  // }
+
+  // )
+  return <h1> Hello, Beautiful World</h1>
+
+
+}
+
+export default Home;
